6e14fdab8b728cc4a4ccbc846940230a9774afec
Lrxc <837533730@qq.com>
2017-06-08 16:09:10 +0800

    two commit

--- app/src/main/java/com/bxlt/customcamera/MainActivity.java
+++ app/src/main/java/com/bxlt/customcamera/MainActivity.java
@@ -1,8 +1,10 @@
 
 package com.bxlt.customcamera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.gotoCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TakePhotoActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bxlt/customcamera/TakePhotoActivity.java
@@ -0,0 +1,121 @@
+package com.bxlt.customcamera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.IOException;
+
+/**
+ * Created by Lrxc on 2017/6/8.
+ */
+
+public class TakePhotoActivity extends AppCompatActivity implements View.OnClickListener {
+    private SurfaceView surfaceView;
+    private Camera camera;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takephoto);
+        initView();
+    }
+
+    private void initView() {
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        findViewById(R.id.takePhoto).setOnClickListener(this);
+
+        surfaceView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceView.getHolder().setFixedSize(176, 144); //设置Surface分辨率
+        surfaceView.getHolder().setKeepScreenOn(true);// 屏幕常亮
+        surfaceView.getHolder().addCallback(new SurfaceCallback());//为SurfaceView的句柄添加一个回调函数
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.takePhoto:
+                if (camera != null) {
+                    camera.takePicture(null, null, new MyPictureCallback());
+                }
+                break;
+        }
+    }
+
+    private class MyPictureCallback implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            camera.startPreview(); // 拍完照后，重新开始预览
+
+            Intent intent = new Intent(TakePhotoActivity.this, ShowImgActivity.class);
+            intent.putExtra("data", data);
+            startActivity(intent);
+        }
+    }
+
+    private class SurfaceCallback implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                // 打开摄像头
+                camera = Camera.open();
+                camera.setPreviewDisplay(holder); // 设置用于显示拍照影像的SurfaceHolder对象
+                camera.setDisplayOrientation(getPreviewDegree(TakePhotoActivity.this));
+                camera.startPreview(); // 开始预览
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();// 获取各项参数
+            parameters.setPictureFormat(PixelFormat.JPEG); // 设置图片格式
+            parameters.setPreviewSize(width, height); // 设置预览大小
+            parameters.setPreviewFrameRate(5);  //设置每秒显示4帧
+            parameters.setPictureSize(width, height); // 设置保存的图片尺寸
+            parameters.setJpegQuality(100); // 设置照片质量
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            if (camera != null) {
+                camera.stopPreview(); // 停止预览
+                camera.release(); // 释放照相机
+                camera = null;
+            }
+        }
+    }
+
+    // 提供一个静态方法，用于根据手机方向获得相机预览画面旋转的角度
+    public static int getPreviewDegree(Activity activity) {
+        // 获得手机的方向
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degree = 0;
+        // 根据手机的方向计算相机预览画面应该选择的角度
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degree = 90;
+                break;
+            case Surface.ROTATION_90:
+                degree = 0;
+                break;
+            case Surface.ROTATION_180:
+                degree = 270;
+                break;
+            case Surface.ROTATION_270:
+                degree = 180;
+                break;
+        }
+        return degree;
+    }
+}
